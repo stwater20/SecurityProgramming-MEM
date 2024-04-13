07265960a5616e8ea8d24f70d41ecbc79ced0fc8
Your Name <you@example.com>
2024-04-13 18:39:45 +0800

    finish mem30-c-1 gdb

---
+++ mem30-c/mem30-c-1-compliant-gdb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int value;
+  struct node *next;
+};
+
+void free_list(struct node *head) {
+  struct node *q;
+  for (struct node *p = head; p != NULL; p = q) {
+    q = p->next;
+    free(p);
+  }
+}
+
+int main() {
+  // Create a list with two nodes for the example
+  struct node *head = malloc(sizeof(struct node));
+  head->value = 1;
+  head->next = malloc(sizeof(struct node));
+  head->next->value = 2;
+  head->next->next = NULL;
+
+  // Free the list
+  free_list(head);
+
+  return 0;
+}
